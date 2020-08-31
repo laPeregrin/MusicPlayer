c233533e023d94e46c772084416c26f5278f9d4b
Vladimir <64407348+laPeregrin@users.noreply.github.com>
2020-08-31 23:56:39 +0300

    Add files via upload

---
+++ MusicPlayerInterface/ViewModels/DataBase.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Win32;
+using MusicPlayerInterface.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicPlayerInterface.Model
+{
+    class DataBase : BindableBase
+    {
+        string[] fileNames;
+        public static IEnumerable<MusicTrack> GetMusicTracks()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+            var res = ofd?.SafeFileName;
+            yield return new MusicTrack { FileName = res };
+        }
+    }
+}
---
+++ MusicPlayerInterface/ViewModels/MusicTrack.cs
@@ -0,0 +1,12 @@
+﻿using MusicPlayerInterface.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicPlayerInterface.Model
+{
+    class MusicTrack : BindableBase
+    {
+        public string FileName { get; set; }
+    }
+}
